b00a96a2b57ccefc58ddba6153775ee0b1e5e0bd
pana-zhu <502285157@qq.com>
2019-09-30 15:23:55 +0800

    修改请求路径kafka1

--- src/main/java/com/example/kafkaDemo/TestProducerController.java
+++ src/main/java/com/example/kafkaDemo/TestProducerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("kafka")
+@RequestMapping("kafka1")
 public class TestProducerController {
 
     @Autowired
